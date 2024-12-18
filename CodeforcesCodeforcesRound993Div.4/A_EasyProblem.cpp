d419681a92bb018f084081fea2e42a86a17b0e82
Shifat <hakim.shifat@gmail.com>
2024-12-18 11:09:10 +0600

    accepted

---
+++ CodeforcesCodeforcesRound993Div.4/A_EasyProblem.cpp
@@ -0,0 +1,26 @@
+// time-limit: 1000
+// problem-url: https://codeforces.com/contest/2044/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define endl "\n"
+#define sp " "
+
+void solve() {
+	int n; cin >> n;
+	int input;
+	while(n--){
+		cin >> input;
+		cout << input-1 << endl;
+	}
+	
+}
+
+int32_t main() {
+  int test = 1;
+  //	scanf("%d", &test);
+  while (test--) {
+    solve();
+  }
+  return 0;
+}
