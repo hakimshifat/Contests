d1b87cf365a638c1532320670caea2ca49280495
Shifat <hakim.shifat@gmail.com>
2024-12-25 23:36:30 +0600

    aa

--- A_BeautifulMatrix.cpp
+++ A_BeautifulMatrix.cpp
@@ -1,33 +1,32 @@
 // time-limit: 2000
 // problem-url: https://codeforces.com/contest/263/problem/A
 #include <bits/stdc++.h>
+#include <cstdint>
 using namespace std;
 #define int long long int
 #define endl "\n"
-void abd()
-{
+void abd() {
 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 }
 
 void solve() {
-	int arr[5][5];
-	for(int i = 0 ; i < 5 ; i++){
-		for(int j = 0 ; j < 5 ;j++){
-			cin >> arr[i][j];
-		}
-	}
-	for(int i = 0 ; i < 5 ; i++){
-		for(int j = 0 ; j < 5 ;j++){
-			if( arr[i][j]==1){
-				cout << abs(2-i) + abs(2-j);
-				break;
-			}
-		}
-	}
-	
+  int arr[5][5];
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
+      cin >> arr[i][j];
+    }
+  }
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
+      if (arr[i][j] == 1) {
+        cout << abs(2 - i) + abs(2 - j);
+        break;
+      }
+    }
+  }
 }
 
 int32_t main() {
