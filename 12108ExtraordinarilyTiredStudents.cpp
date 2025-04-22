411b03158121b10f84bc1f9ac256cb3ad47be4a4
Shifat <hakim.shifat@gmail.com>
2025-04-22 10:11:20 +0600

    got back to zero

--- 12108ExtraordinarilyTiredStudents.cpp
+++ 12108ExtraordinarilyTiredStudents.cpp
@@ -10,18 +10,6 @@ using namespace std;
 const int MOD = 1e9 + 7;
 const int maxN = 1e5 + 5;
 
-long long power(long long base, long long exp) {
-  long long res = 1;
-  base %= MOD;
-  while (exp > 0) {
-    if (exp % 2 == 1)
-      res = (res * base) % MOD;
-    base = (base * base) % MOD;
-    exp /= 2;
-  }
-  return res;
-}
-
 #define fastio                                                                 \
   ios_base::sync_with_stdio(false);                                            \
   cin.tie(nullptr)
