4e69859fb3d4763701ef4fdda0c7b2cc48e3b037
Shifat <hakim.shifat@gmail.com>
2025-05-13 18:50:44 +0600

    again

--- cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Delete files with extensions matching in*, ans*, or out*
-find . -type f \( -name "*.in*" -o -name "*.ans*" -o -name "*.out*" -o -name "*.exe*" \) -exec rm -v {} \;
-
-echo "Deleted all files with extensions matching 'in', 'ans', or 'out'."
-
